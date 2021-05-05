0a72a929c0735513060ad1cdbf494c2b1ca82275
MatrixMike <matroxmike@icloud.com>
2021-05-05 18:45:08 +1000

    working with snackbar

--- app/src/main/java/com/example/basicact1/MainActivity.kt
+++ app/src/main/java/com/example/basicact1/MainActivity.kt
@@ -9,14 +9,14 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var count = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, getString(R.string.snackString) + " x " + count++, Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
