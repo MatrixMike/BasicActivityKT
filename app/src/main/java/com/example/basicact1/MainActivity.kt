73432adb6a64eaaaf84c30d0f0e64a43baadb2f7
MatrixMike <matroxmike@icloud.com>
2022-08-15 18:44:34 +1000

    working with snackbar D - problems with kotlin number formatting J

--- app/src/main/java/com/example/basicact1/MainActivity.kt
+++ app/src/main/java/com/example/basicact1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.basicact1
 // 10/6/2020
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
@@ -37,6 +38,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var TAG: String = "MikeH"
+        Log.i(TAG, "onOptionsItemSelected")
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
